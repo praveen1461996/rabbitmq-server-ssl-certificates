f0eeb7185f195b098e9ad0a6b00fee240f1e7680
praveen1461996 <42828348+praveen1461996@users.noreply.github.com>
2019-03-29 15:13:35 +0530

    Update prepare-server.sh

--- prepare-server.sh
+++ prepare-server.sh
@@ -10,7 +10,7 @@ cd /home/testca
 
 # Create a self-signed certificate that will serve a certificate authority (CA).
 # The private key is located under "private".
-openssl req -x509 -config openssl.cnf -newkey rsa:2048 -days 365 -out cacert.pem -outform PEM -subj /CN=test.xpms.ai/ -nodes
+openssl req -x509 -config openssl.cnf -newkey rsa:2048 -days 365 -out cacert.pem -outform PEM -subj /CN=$(hostname)/ -nodes
 
 # Encode our certificate with DER.
 openssl x509 -in cacert.pem -out cacert.cer -outform DER
