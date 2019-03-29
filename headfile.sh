3994ff79d52a4f51d8b78bbca4416349ad4858a4
praveen1461996 <42828348+praveen1461996@users.noreply.github.com>
2019-03-29 15:06:46 +0530

    Update headfile.sh

--- headfile.sh
+++ headfile.sh
@@ -1,11 +1,36 @@
 #!/bin/sh
 
-#installing openssl
-sudo apt-get install openssl -y 
+#installing openssl to create server client certificates
+sudo apt-get install openssl -y
+
+#locations for our certificates
 mkdir -p /home/testca/certs
 mkdir -p /home/testca/private
 mkdir -p /home/server
+mkdir -p /home/client
+
+#making private foulder secure
 chmod 700 /home/testca/private
+
+
 echo 01 > /home/testca/serial
 touch /home/testca/index.txt
 touch /home/testca/index.txt.attr
+
+#changing scripts permissions to exec mode 
+chmod +x /home/prepare-server.sh /home/generate-client-keys.sh rabbitmq.sh
+
+#preparing server certificates
+./prepare-server.sh
+
+#preparing client certificats
+./generate-client-keys.sh
+
+#installing rabbitmq
+./rabbitmq.sh
+
+#deploying configuration file 
+cp rabbitmq.config /etc/rabbitmq/rabbitmq.config
+
+#restarting the server
+sudo systemctl restart rabbitmq-server
